f4c49f071646745100ab204468b666ca182f80b1
tomoro <1845081t@stu.kobe-u.ac.jp>
2022-08-05 16:41:16 +0900

    テストページ表示できるように

---
+++ miraiSugoroku/src/main/java/ksp/group3/miraiSugoroku/controller/UserController.java
@@ -0,0 +1,13 @@
+package ksp.group3.miraiSugoroku.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    
+    @GetMapping("/")
+    public String showIndexPage(){
+        return "index";
+    }
+}
