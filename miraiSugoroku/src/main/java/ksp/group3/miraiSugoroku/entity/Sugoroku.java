d27543b06dbb31156979db8d1c01764db088e18f
Raystar247 <24star10@gmail.com>
2022-08-08 14:05:01 +0900

    add GameConfigForm class

--- miraiSugoroku/src/main/java/ksp/group3/miraiSugoroku/entity/Sugoroku.java
+++ miraiSugoroku/src/main/java/ksp/group3/miraiSugoroku/entity/Sugoroku.java
@@ -5,16 +5,20 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Sugoroku {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long sugorokuId; // すごろくのID
-    int nowPlayer; // 現在行動しているplaylerの順番
+    int nowPlayer; // 現在行動しているplayerの順番
     int nPlayers; // playerの数
     int length; // すごろくが持っているマスの数（ゴール・スタートは除く）
 
