5be14cbeff4d9547b84b6f4597926542a7bbc8b5
rmatsumoto <rmatsumoto@fine.cs.kobe-u.ac.jp>
2022-08-08 12:04:40 +0900

    Square関連

---
+++ miraiSugoroku/src/main/java/ksp/group3/miraiSugoroku/entity/Square.java
@@ -0,0 +1,25 @@
+package ksp.group3.miraiSugoroku.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Square {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long squareId;
+    String title;
+    Long squareEventId;
+    String description;
+    Long creatorId;
+    boolean isApproved;
+}
