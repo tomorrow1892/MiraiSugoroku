d27085fd35ab77ced2bd691c4de5b375d15bb8bd
Horie <h.horie916@gmail.com>
2022-08-08 10:46:05 +0900

    コントローラ作成

--- miraiSugoroku/src/main/java/ksp/group3/miraiSugoroku/controller/UserController.java
+++ miraiSugoroku/src/main/java/ksp/group3/miraiSugoroku/controller/UserController.java
@@ -5,9 +5,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
-    
+    // @Autowired
+    // SquareRepository sRepo;
+
     @GetMapping("/")
-    public String showIndexPage(){
+    public String showIndexPage() {
         return "index";
     }
+
+    @GetMapping("/menu")
+    public String showGuestMenuPage() {
+        return "guest_menu.html";
+    }
+
+    @GetMapping("/squares")
+    public String showGuestSquareListPage() {
+        return "guest_squarelist";
+    }
+
+    @GetMapping("/config")
+    public String showGuestSugorokuConfigPage() {
+        return "guest_sugoroku_config";
+    }
+
+    @GetMapping("/sugoroku")
+    public String showSugorokuPage() {
+        return "sugoroku";
+    }
+
 }
