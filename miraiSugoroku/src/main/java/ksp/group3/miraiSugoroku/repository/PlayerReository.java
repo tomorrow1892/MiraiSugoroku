cbf17205913f6bd857c467b46f9c8b5a740c34ea
Raystar247 <24star10@gmail.com>
2022-08-08 11:02:33 +0900

    Add repositories about playing game

--- miraiSugoroku/src/main/java/ksp/group3/miraiSugoroku/repository/PlayerReository.java
+++ miraiSugoroku/src/main/java/ksp/group3/miraiSugoroku/repository/PlayerReository.java
@@ -1,5 +1,17 @@
 package ksp.group3.miraiSugoroku.repository;
 
-public class PlayerReository {
-    
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import ksp.group3.miraiSugoroku.entity.Player;
+
+@Repository
+public interface PlayerReository extends CrudRepository<Player, Long> {
+
+    List<Player> findBySugorokuId(Long sugorokuId);
+
+    List<Player> findBySugorokuIdAndOrder(Long sugorokuoId, int order);
+
 }
