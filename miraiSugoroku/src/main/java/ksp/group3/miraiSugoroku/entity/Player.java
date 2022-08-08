69c1f45bde40c91e395eeb617826c1b318af42f4
tomoro <1845081t@stu.kobe-u.ac.jp>
2022-08-08 09:48:27 +0900

    ゲームのテスト画面表示まで

--- miraiSugoroku/src/main/java/ksp/group3/miraiSugoroku/entity/Player.java
+++ miraiSugoroku/src/main/java/ksp/group3/miraiSugoroku/entity/Player.java
@@ -1,5 +1,18 @@
 package ksp.group3.miraiSugoroku.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Player {
-    
+    int playerId;
+    // Long sugorokuId;
+    // String icon;
+    String name;
+    // int order;
+    // Boolean isGoaled;
+    int points;
+    int position;
+    Boolean isBreak;
 }
