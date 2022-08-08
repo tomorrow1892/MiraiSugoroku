e18cd790ffeb61da5be93d14ad054a68d30c6c5d
tomoro <1845081t@stu.kobe-u.ac.jp>
2022-08-08 09:38:14 +0900

    ゲーム画面のテスト表示まで

--- miraiSugoroku/src/main/java/ksp/group3/miraiSugoroku/controller/GameRestController.java
+++ miraiSugoroku/src/main/java/ksp/group3/miraiSugoroku/controller/GameRestController.java
@@ -1,5 +1,28 @@
 package ksp.group3.miraiSugoroku.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import ksp.group3.miraiSugoroku.entity.Player;
+
+@RestController
 public class GameRestController {
-    
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @GetMapping("/api/diceRoll")
+    String diceRoll(@RequestParam String suzi) throws JsonProcessingException {
+
+        System.out.println(suzi);
+        return objectMapper.writeValueAsString(new Player(1, "さんだ", 100, 1 + Integer.parseInt(suzi), false));
+    }
+
+    @GetMapping("/api/doEvent")
+    String doEvent() throws JsonProcessingException {
+        return objectMapper.writeValueAsString(new Player(1, "さんだ", 200, 8, false));
+    }
 }
